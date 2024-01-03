d3d3a27e3657382e924e86799cbf7eedf6b6382d
Ararat0Grigoryan <arogrigoryan0203@gmail.com>
2024-01-03 21:16:00 +0400

    proces multilanguage

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function update_position(e) {
-	let _t = e.target;
-	
-	if(_t.tagName.match(/^button$/i)) {
-		let r = _t.getBoundingClientRect();
-		['x', 'y'].forEach(c => 
-			_t.style.setProperty(`--${c}`, 
-													 `${e[`client${c.toUpperCase()}`] - r[c]}px`))
-	}
+  let _t = e.target;
+
+  if (_t.tagName.match(/^button$/i)) {
+    let r = _t.getBoundingClientRect();
+    ["x", "y"].forEach((c) =>
+      _t.style.setProperty(
+        `--${c}`,
+        `${e[`client${c.toUpperCase()}`] - r[c]}px`
+      )
+    );
+  }
 }
 
-addEventListener('mouseover', update_position);
-addEventListener('mouseout', update_position)
+addEventListener("mouseover", update_position);
+addEventListener("mouseout", update_position);
+
+document.addEventListener("click", function (event) {
+  if (!event.target.matches(".menu-item-scroll")) return;
+
+  event.preventDefault();
+
+  const element = document.getElementById(event.target.dataset.target);
+
+  element.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+  });
+});
+
+document.getElementById("main-action-button").onclick = function () {
+  document.getElementById("about-me").scrollIntoView({ behavior: "smooth" });
+};
